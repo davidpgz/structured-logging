235b38f6f27451608ab19a189581e776efaa79a7
David Poulin-Gagnon <dpoulingagnon@nexapp.ca>
2022-09-29 21:39:41 -0400

    Remove fetch-job for simplicity

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import express from 'express';
-import { fetchJob } from './fetch-job';
+import { randomDelay } from './random-delay';
 import SimpleLogger from './simple-logger';
 
-export const jobs = ['job 3','job 5','job 7']
+const jobs = ['job 3','job 5','job 7']
 
-export const logger = new SimpleLogger();
+const logger = new SimpleLogger();
 const app = express();
 
 app.use('/jobs/:name', async function(req, res, next){
@@ -18,6 +18,7 @@ app.use('/jobs/:name', async function(req, res, next){
     } catch (error) {
         logger.log(error);
         res.status(500).send({ message: "We're down..."})
+        logger.log(`Request status 500`);
     }
 });
 
@@ -25,5 +26,27 @@ app.listen(8080, function(){
     console.log('We are open for business!');
 });
 
+async function fetchJob(jobName: string) {
+    logger.log("Fetching Job...");
 
+    // to simulate fetching
+    await randomDelay();
+    logger.log('Checking for failure');
+    await randomDelay();
 
+    const failingJob = Math.round(Math.random() * 2);
+    if (+jobName === failingJob) {
+        throw Error("We're going down!!!");
+    };
+
+    const displayName = jobs[+jobName];
+    logger.log(`Display name ${displayName}`);
+    await randomDelay();
+
+    logger.log(`Fetched job ${jobName}`);
+
+    return {
+        name: jobName,
+        displayName,
+    };
+}
